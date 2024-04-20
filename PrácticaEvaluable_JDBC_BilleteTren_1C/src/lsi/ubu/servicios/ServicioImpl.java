a499f1366fd6e5ea5c12bccabcc91f8c4fc8fc66
Martín González Saiz <160163628+martingonzsaiz@users.noreply.github.com>
2024-04-20 20:23:13 +0200

    Implmentación de comprarBillete y anularBillete con sus excepciones

--- PrácticaEvaluable_JDBC_BilleteTren_1C/src/lsi/ubu/servicios/ServicioImpl.java
+++ PrácticaEvaluable_JDBC_BilleteTren_1C/src/lsi/ubu/servicios/ServicioImpl.java
@@ -1,5 +1,6 @@
 package lsi.ubu.servicios;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,6 +12,7 @@ import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import lsi.ubu.excepciones.CompraBilleteTrenException;
 import lsi.ubu.util.PoolDeConexiones;
 
 public class ServicioImpl implements Servicio {
@@ -19,115 +21,200 @@ public class ServicioImpl implements Servicio {
 	@Override
 	public void anularBillete(Time hora, java.util.Date fecha, String origen, String destino, int nroPlazas, int ticket)
 			throws SQLException {
+		Connection conn = null;
 		PoolDeConexiones pool = PoolDeConexiones.getInstance();
-
-		/* Conversiones de fechas y horas */
+		PreparedStatement stDatos = null;
+		PreparedStatement stUpdate = null;
+		PreparedStatement stDelete = null;
+		int nRowsDeleted = 0;
+		
 		java.sql.Date fechaSqlDate = new java.sql.Date(fecha.getTime());
 		java.sql.Timestamp horaTimestamp = new java.sql.Timestamp(hora.getTime());
-
-		Connection con = null;
-		PreparedStatement st = null;
+		
 		ResultSet rs = null;
-
-		// A completar por el alumno
-	}
-
-	@Override
-	public void comprarBillete(Time hora, Date fecha, String origen, String destino, int nroPlazas)
-			throws SQLException {
-	    LOGGER.info("Obteniendo ID de viaje para Hora: {}, Origen: {}, Destino: {}", hora, origen, destino);
-		Connection conn = null;
-		PreparedStatement st = null;
-		ResultSet rs = null;
-		int contador = 1;
+		int rsRows = 0;
 		int idViaje = 0;
-		int precio = 0;
 		
 		try {
-			PoolDeConexiones pool = PoolDeConexiones.getInstance();
 			conn = pool.getConnection();
-			conn.setAutoCommit(false);
+			String query = 
+					"SELECT v.idViaje, t.cantidad "
+					+ "FROM viajes v "
+					+ "JOIN tickets t ON t.idViaje = v.idViaje "
+					+ "WHERE idTicket = ?";
+			stDatos = conn.prepareStatement(query);
+			stDatos.setInt(1, ticket);
 			
-			/* Conversiones de fechas y horas */
-			java.sql.Date fechaSqlDate = new java.sql.Date(fecha.getTime());
-			java.sql.Timestamp horaTimestamp = new java.sql.Timestamp(hora.getTime());
+			rs = stDatos.executeQuery();
 			
-			idViaje = obtenerIdViaje(conn, hora, origen, destino);
-			
-		    String query = "INSERT INTO tickets (idTicket, idViaje, fechaCompra, cantidad, precio) VALUES (seq_tickets.NEXTVAL, ?, ?, ?, ?)";
-	
-			st = conn.prepareStatement(query);
-			
-			precio = nroPlazas * obtenerPrecio(conn, hora, origen, destino);
-			
-			st.setInt(contador++, idViaje);
-			st.setDate(contador++, fechaSqlDate);
-			st.setInt(contador++, nroPlazas);
-			st.setInt(contador, precio);
-			
-			st.executeUpdate();
-
-		}catch(SQLException e){
-			if(conn == null) {
-				conn.rollback();
+			if(rs.next()) {
+				if(nroPlazas < rs.getInt("cantidad")) {
+					idViaje = rs.getInt("idViaje");
+					String update = 
+							"UPDATE viajes SET nPlazasLibres = nPlazasLibres + ? WHERE idViaje = ?";
+					
+					stUpdate = conn.prepareStatement(update);
+					stUpdate.setInt(1, nroPlazas);
+					stUpdate.setInt(1, idViaje);
+					
+					stUpdate.executeUpdate();
+					
+					String delete =
+							"DELETE FROM tickets WHERE id = ?";
+					
+					stDelete = conn.prepareStatement(delete);
+					stDelete.setInt(1, ticket);
+					
+					nRowsDeleted = stDelete.executeUpdate();
+					
+					if(nRowsDeleted == 0) {
+						throw new CompraBilleteTrenException(CompraBilleteTrenException.NO_ANULADO);
+					}
+					
+				}
+				else {
+					throw new CompraBilleteTrenException(CompraBilleteTrenException.NO_EXISTE_TICKET_ANULAR);
+				}
 			}
-			throw e;
+			
+		}catch(SQLException e){
+			conn.rollback();
+			if(e instanceof CompraBilleteTrenException) {
+				throw e;
+			}else {
+				LOGGER.error(e.getMessage());
+			}
+			
 		}
 		finally {
 			if(conn != null) {
 				conn.setAutoCommit(true);
 				conn.close();
 			}
-			
-		}
-	}
-	
-	public int obtenerIdViaje(Connection conn, Time hora, String origen, String destino) throws SQLException {
-	    LOGGER.info("Obteniendo ID de viaje para Hora: {}, Origen: {}, Destino: {}", hora, origen, destino);
-	    String query = "SELECT v.idViaje FROM recorridos r JOIN viajes v ON v.idRecorrido = r.idRecorrido WHERE r.horaSalida = ? AND r.estacionOrigen = ? AND r.estacionDestino = ?";
-	    try (PreparedStatement st = conn.prepareStatement(query)) {
-	        st.setTime(1, hora); 
-	        st.setString(2, origen); 
-	        st.setString(3, destino); 
-	        try (ResultSet rs = st.executeQuery()) {
-	            if (rs.next()) {
-	                return rs.getInt("idViaje");
-	            } else {
-	                throw new SQLException("No existe viaje para tal fecha, origen y destino.");
-	            }
-	        }
-	    } catch (SQLException e) {
-	        LOGGER.error("Error obteniendo el ID del viaje", e);
-	        throw e;
-	    }
-	}
-
-	
-	public int obtenerPrecio(Connection conn, Time hora, String origen, String destino) throws SQLException{
-		PreparedStatement st = null;
-		ResultSet rs = null;
-		int precio = 0;
-		int contador = 1;
-			    
-		try {
-		    String viaje = "SELECT recorridos.precio FROM recorridos WHERE horaSalida = ? AND estacionOrigen = ? AND estacionDestino = ?";
-			st = conn.prepareStatement(viaje);
-			java.sql.Timestamp horaTimestamp = new java.sql.Timestamp(hora.getTime());
-			st.setString(contador++, origen);
-			st.setString(contador++, destino);
-			
-			rs = st.executeQuery();
-			
-			if (rs.next()) {
-			    precio = rs.getInt("precio");
-			} else {
-			    throw new SQLException("No existe ese recorrido.");
+			else if(rs == null) {
+				conn.setAutoCommit(true);
+				rs.close();
 			}
-		} catch (SQLException e) {
-			LOGGER.error("Error fetching viaje ID", e);
-	        throw e;
+			else if(stDatos == null) {
+				conn.setAutoCommit(true);
+				stDatos.close();
+			}
+			else if(stUpdate == null) {
+				conn.setAutoCommit(true);
+				stUpdate.close();
+			}
+			else if(stDelete == null) {
+				conn.setAutoCommit(true);
+				stDelete.close();
+			}
 		}
-		return precio;
+		
+		
 	}
 
+	@Override
+	public void comprarBillete(Time hora, Date fecha, String origen, String destino, int nroPlazas)
+			throws SQLException {
+		Connection conn = null;
+		PoolDeConexiones pool = PoolDeConexiones.getInstance();
+		PreparedStatement stDatos = null;
+		PreparedStatement stUpdate = null;
+		PreparedStatement stInsert = null;
+		
+		java.sql.Date fechaSqlDate = new java.sql.Date(fecha.getTime());
+		java.sql.Timestamp horaTimestamp = new java.sql.Timestamp(hora.getTime());
+		
+		ResultSet rs = null;
+		int nRowsUpdated = 0;
+		int idViaje = 0;
+		
+		try {
+			conn = pool.getConnection();
+			conn.setAutoCommit(false);
+			
+			LOGGER.info("Parámetros - Origen: {}, Destino: {}, Hora: {}, Fecha: {}", origen, destino, horaTimestamp, fechaSqlDate);
+			
+			int contador = 1;
+			String query = 
+					"SELECT v.idViaje, r.precio "
+					+ "FROM recorridos r "
+					+ "JOIN viajes v ON r.idRecorrido = v.idRecorrido "
+					+ "WHERE r.horasalida - TRUNC(r.horaSalida) = ? - TRUNC(?) AND v.fecha = ? AND r.estacionOrigen = ? AND r.estacionDestino = ?";
+			
+			stDatos = conn.prepareStatement(query);
+			stDatos.setTimestamp(contador++, horaTimestamp);
+			stDatos.setTimestamp(contador++, horaTimestamp);
+			stDatos.setDate(contador++, fechaSqlDate);
+			stDatos.setString(contador++, origen); 
+			stDatos.setString(contador++, destino);
+	        
+	        rs = stDatos.executeQuery();
+			
+	        if(rs.next()) {
+	        	idViaje = rs.getInt("idViaje");
+	        	
+	        	contador = 1;
+		        String restarPlazas = 
+		        		"UPDATE viajes SET nPlazasLibres = nPlazasLibres - ? WHERE idViaje = ? AND nPlazasLibres >= ?";
+		        		
+		        stUpdate = conn.prepareStatement(restarPlazas);
+		        stUpdate.setInt(contador++, nroPlazas);
+		        stUpdate.setInt(contador++, idViaje);
+		        stUpdate.setInt(contador++, nroPlazas);
+		        
+		        nRowsUpdated= stUpdate.executeUpdate();
+		        
+		        if(nRowsUpdated > 0) {
+		        	contador = 1;
+		        	BigDecimal precio = rs.getBigDecimal("precio");
+		        	BigDecimal precioTotal = precio.multiply(new BigDecimal(nroPlazas));
+				    String insertTicket = "INSERT INTO tickets (idTicket, idViaje, fechaCompra, cantidad, precio) VALUES (seq_tickets.NEXTVAL, ?, TRUNC(current_date), ?, ?)";
+			
+				    stInsert = conn.prepareStatement(insertTicket);
+									
+					stInsert.setInt(contador++, idViaje);
+					stInsert.setInt(contador++, nroPlazas);
+					stInsert.setBigDecimal(contador, precioTotal);
+					
+					stInsert.executeUpdate();
+		        }else{
+		        	throw new CompraBilleteTrenException(CompraBilleteTrenException.NO_PLAZAS);
+		        }
+		        
+		    }else {
+		    	throw new CompraBilleteTrenException(CompraBilleteTrenException.NO_EXISTE_VIAJE);
+	        }
+
+		}catch(SQLException e){
+			conn.rollback();
+			if(e instanceof CompraBilleteTrenException) {
+				throw e;
+			}else {
+				LOGGER.error(e.getMessage());
+			}
+			
+		}
+		finally {
+			if(conn != null) {
+				conn.setAutoCommit(true);
+				conn.close();
+			}
+			else if(rs == null) {
+				conn.setAutoCommit(true);
+				rs.close();
+			}
+			else if(stDatos == null) {
+				conn.setAutoCommit(true);
+				stDatos.close();
+			}
+			else if(stUpdate == null) {
+				conn.setAutoCommit(true);
+				stUpdate.close();
+			}
+			else if(stInsert == null) {
+				conn.setAutoCommit(true);
+				stInsert.close();
+			}
+		}
+	}
 }
